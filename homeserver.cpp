4698d2e99727866025f8ca055a1f96217f52f0c5
2AECML <805284158@qq.com>
2024-08-23 22:00:40 +0800

    使用分隔符来分割json

--- homeserver.cpp
+++ homeserver.cpp
@@ -59,21 +59,44 @@ void HomeServer::onSocketStateChanged(QAbstractSocket::SocketState socketState) 
 }
 
 void HomeServer::processData(QTcpSocket* socket, const QByteArray& data) {
-    QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
-    QJsonObject json = jsonDoc.object();
-    if (json["type"] == "user_list") {
-        QList<BasicUserInfo> list = DatabaseManager::getUserList();
-        sendUserList(socket, list);
-    }
-    else if (json["type"] == "user_info") {
-        QString accountID = json["id"].toString();
-        BasicUserInfo info = DatabaseManager::getUserInfo(accountID);
-        sendUserInfo(socket, info);
-    }
-    else {
-        qDebug() << "HomeServer: An unknown type of json was received: " << data;
+    // 静态缓冲区，用于存储接收到的数据
+    static QByteArray buffer;
+
+    // 将新接收到的数据追加到缓冲区
+    buffer.append(data);
+
+    // 处理缓冲区中的数据，直到找不到分隔符为止
+    while (buffer.contains("\r\n\r\n")) {
+        // 找到分隔符的位置
+        int index = buffer.indexOf("\r\n\r\n");
+
+        // 提取分隔符之前的部分作为一个完整的消息
+        QByteArray message = buffer.left(index).trimmed();
+
+        // 从缓冲区中移除已经处理的消息
+        buffer.remove(0, index + 4);
+
+        // 解析 JSON 消息
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(message);
+        QJsonObject json = jsonDoc.object();
+
+        // 根据消息类型进行处理
+        if (json["type"] == "user_list") {
+            // 如果消息类型是 "user_list"，从数据库获取用户列表并发送回客户端
+            QList<BasicUserInfo> list = DatabaseManager::getUserList();
+            sendUserList(socket, list);
+        } else if (json["type"] == "user_info") {
+            // 如果消息类型是 "user_info"，从数据库获取用户信息并发送回客户端
+            QString accountID = json["id"].toString();
+            BasicUserInfo info = DatabaseManager::getUserInfo(accountID);
+            sendUserInfo(socket, info);
+        } else {
+            // 如果消息类型未知，记录调试信息
+            qDebug() << "HomeServer: An unknown type of json was received: " << message;
+        }
     }
 }
+
 
 void HomeServer::sendUserList(QTcpSocket* socket, const QList<BasicUserInfo>& list) {
     QJsonArray jsonArray;
